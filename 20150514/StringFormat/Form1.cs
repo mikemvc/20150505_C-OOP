2f5a013bf37c35e9c9917ec72a21571aeca38cf4
mikemvc <mikemvc@hotmail.com>
2015-05-18 10:35:30 +0800

    string.Format() 1.

---
+++ 20150514/StringFormat/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringFormat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 100;
+            int y = 200;
+            // (100, 200);
+
+            //string result = "";
+            //result = "(" + x.ToString() + ", " + y.ToString() + ")";
+            //button1.Text = result;
+            string result = "";
+            // result = string.Format("format", value1, value2, ....)
+            result = string.Format("({0}, {1})", x, y);
+            button1.Text = result;
+        }
+    }
+}
