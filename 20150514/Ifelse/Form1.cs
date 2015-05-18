153f6b78ea94059dd754c0d24b629504497431eb
mikemvc <mikemvc@hotmail.com>
2015-05-18 09:50:30 +0800

    Ifelse

---
+++ 20150514/Ifelse/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ifelse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                button1.Text = "Yes";
+            else
+                button1.Text = "No";
+
+        }
+    }
+}
