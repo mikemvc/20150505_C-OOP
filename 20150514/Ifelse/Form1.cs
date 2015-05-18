bdb961b6f045db2c00e1edf4b15323a2708875bc
mikemvc <mikemvc@hotmail.com>
2015-05-18 09:52:16 +0800

    ifelse Sample

--- 20150514/Ifelse/Form1.cs
+++ 20150514/Ifelse/Form1.cs
@@ -24,6 +24,18 @@ namespace Ifelse
             else
                 button1.Text = "No";
 
+            #region 示範
+            // 如果IF有多行敘述，則要用{}大括弧包起來，
+            // 只有單行則可以省略
+
+            //int x = -10;            
+            //if (x >= 0) {
+            //    button1.Text = "Yes";
+            //    this.Text = "OK";
+            //}
+            //else // if (x < 0)
+            //    button1.Text = "NO";
+            #endregion
         }
     }
 }
