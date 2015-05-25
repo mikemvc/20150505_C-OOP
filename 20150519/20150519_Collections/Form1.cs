c2dee086c2be279967741d8f032817587979bf0e
mikemvc <mikemvc@hotmail.com>
2015-05-25 09:38:44 +0800

    ArrayList

---
+++ 20150519/20150519_Collections/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20150519_Collections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList bag = new ArrayList();
+            bag.Add(100);
+            bag.Add("Chien");
+            bag.Add(65.2);
+
+            button1.Text = bag.Count.ToString();  // 3
+            button1.Text = bag[1].ToString();  // Chien
+
+        }
+    }
+}
