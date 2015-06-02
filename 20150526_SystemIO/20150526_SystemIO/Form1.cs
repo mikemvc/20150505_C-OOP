6653e35d5302245885f75f543861d3c33e591842
mikemvc <mikemvc@hotmail.com>
2015-06-02 08:53:31 +0800

    CreateDirectory

---
+++ 20150526_SystemIO/20150526_SystemIO/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20150526_SystemIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // using System.IO;
+            Directory.CreateDirectory(@"C:\test123\test456\test789");
+            button1.Text = "OK";
+        }
+    }
+}
