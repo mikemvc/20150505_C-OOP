4188385568ed0357b66ae53ecba27fdaa8c9c400
mikemvc <mikemvc@hotmail.com>
2015-06-05 08:47:48 +0800

    Overloading

--- 20150521/20150521_functions/Form1.cs
+++ 20150521/20150521_functions/Form1.cs
@@ -28,6 +28,88 @@ namespace _20150521_functions
             button1.Text = answer.ToString();
         }
 
+        //void ShowStar(int iHowMany, string sWhat = "*") {
+        //    string sStar = "";
+        //    for (int i = 1; i <= iHowMany; i++) {
+        //        sStar += sWhat;
+        //    }
+        //    MessageBox.Show(sStar);
+        //}
+
+        // Overloading
+        void ShowStar()
+        {
+            ShowStar(3, "*");
+        }
+
+        void ShowStar(int iHowMany)
+        {
+            ShowStar(iHowMany, "*");
+        }
+
+        //void ShowStar(int iHowMany, string sWhat) {
+        //    string sStar = "";
+        //    for (int i = 1; i <= iHowMany; i++) {
+        //        sStar += sWhat;
+        //    }
+        //    MessageBox.Show(sStar);
+        //}
+
+        //void ShowStar(int iHowMany, string sWhat) {
+        //    if (iHowMany > 0) {
+        //        if (iHowMany <= 20) {
+        //            string sStar = "";
+        //            for (int i = 1; i <= iHowMany; i++) {
+        //                sStar += sWhat;
+        //            }
+        //            MessageBox.Show(sStar);
+        //        }
+        //        else
+        //            MessageBox.Show("iHowMany <= 20 please.");
+        //    }
+        //    else
+        //        MessageBox.Show("iHowMany > 0 please.");
+        //}
+        
+        void ShowStar(int iHowMany, string sWhat)
+        {
+            if (iHowMany > 20)
+            {
+                MessageBox.Show("iHowMany <= 20 please.");
+                return;
+            }
+            if (iHowMany <= 0)
+            {
+                MessageBox.Show("iHowMany > 0 please.");
+                return;
+            }
+            if (iHowMany == 4)
+            {
+                MessageBox.Show("iHowMany != 4 please.");
+                return;
+            }
+
+            string sStar = "";
+            for (int i = 1; i <= iHowMany; i++)
+            {
+                sStar += sWhat;
+            }
+            MessageBox.Show(sStar);
+
+            // finally..
+        }
+
+        // Overloading...
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // ShowStar(5, "%");
+            // ShowStar(5);
+            // ShowStar();
+
+            int iCount = 3;
+            ShowStar(iCount);
+        }
+
         
     }
 }
