52a9e4cf4b347203f34ffce615e8fb17517ee8e7
mikemvc <mikemvc@hotmail.com>
2015-06-05 09:13:28 +0800

    string.Length

---
+++ 20150521/20150521_StringFunctions/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20150521_StringFunctions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // string s = "ABC";  // 3
+            string s = "ABC中文字";  // 6
+            // System.Text.Encoding.UTF8.GetBytes()
+            // Sytem.Text.Encoding.GetEncoding("big5")
+            int iLength = s.Length;
+            button1.Text = iLength.ToString();
+        }
+    }
+}
